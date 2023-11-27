47b2d31e6c7355dde587db4cbe1629c61c8a1a7a
Meaqua <lzlloveyou@vip.qq.com>
2023-11-27 23:40:46 +0800

    feat: 打字机

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,67 @@
 <template>
-  <div>
-    <h1 class="text-2xl font-bold underline">hello</h1>
-  </div>
+      <h1
+      class="mx-auto max-w-5xl font-display text-3xl font-bold tracking-normal text-slate-900 sm:text-7xl"
+    >
+      让每张照片，都有故事可<strong class="bg-pink-100">讲</strong>
+      <div class="mt-4">
+        AI 一键生成
+        <!-- -->
+        <span class="relative whitespace-nowrap text-pink-400">
+          <svg
+            aria-hidden="true"
+            viewBox="0 0 418 42"
+            class="absolute top-2/3 left-0 h-[0.58em] w-full fill-pink-400/70"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"
+            ></path>
+          </svg>
+          <span
+            class="index-module_type__E-SaG relative text-pink-400"
+            v-for="(letter, index) in displayedText"
+            :key="index"
+            >{{ letter }}</span
+          >
+        </span>
+        文案
+      </div>
+    </h1>
 </template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
+const words = ref(["小红书", "微博", "朋友圈"]);
+const displayedText = ref("");
+let wordIndex = ref(0);
+let letterIndex = ref(0);
+
+const typeWriterEffect = () => {
+  const typingDelay = 200; // 打字间隔时间
+  const pauseDuration = 1200; // 一个词完整出现后的停顿时间
+
+  const typeNextLetter = () => {
+    const word = words.value[wordIndex.value];
+
+    if (letterIndex.value < word.length) {
+      displayedText.value += word[letterIndex.value];
+      letterIndex.value++;
+      setTimeout(typeNextLetter, typingDelay);
+    } else {
+      // 停顿一定时间后，移动到下一个词
+      setTimeout(() => {
+        wordIndex.value = (wordIndex.value + 1) % words.value.length;
+        letterIndex.value = 0;
+        displayedText.value = "";
+        typeNextLetter(); // 继续打下一个词
+      }, pauseDuration);
+    }
+  };
+
+  typeNextLetter(); // 开始打字
+};
+
+onMounted(() => {
+  typeWriterEffect();
+});
+</script>
